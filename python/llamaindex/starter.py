651ecc2574a4fb246223de5c83d0b3cb8990798f
chrucci <chrucci_github@spamex.com>
2023-12-04 18:28:43 -0500

    Adding LlamaIndex files

---
+++ python/llamaindex/starter.py
@@ -0,0 +1,32 @@
+import logging
+import sys
+import os.path
+from llama_index import (
+    VectorStoreIndex,
+    SimpleDirectoryReader,
+    StorageContext,
+    load_index_from_storage,
+    ServiceContext,
+)
+
+logging.basicConfig(stream=sys.stdout, level=logging.INFO)
+logging.getLogger().addHandler(logging.StreamHandler(stream=sys.stdout))
+
+service_context = ServiceContext.from_defaults(chunk_size=1000)
+
+# check if storage already exists
+if not os.path.exists("./storage"):
+    # load the documents and create the index
+    documents = SimpleDirectoryReader("data").load_data()
+    index = VectorStoreIndex.from_documents(documents, service_context=service_context)
+    # store it for later
+    index.storage_context.persist()
+else:
+    # load the existing index
+    storage_context = StorageContext.from_defaults(persist_dir="./storage")
+    index = load_index_from_storage(storage_context, service_context=service_context)
+
+# either way we can now query the index
+query_engine = index.as_query_engine()
+response = query_engine.query("What did the author do growing up?")
+print(response)
